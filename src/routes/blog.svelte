1f91f8505e87bd47f758c4f2bf667c15c43d43b6
Clayton Cook <claytonleonardcook@gmail.com>
2021-10-01 16:31:30 -0400

    updated content and styling

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -6,8 +6,6 @@
     <title>Clayton Leonard Cook | Blog</title>
 </svelte:head>
 
-<h1>Blog</h1>
-
 <Window title="Default" tags={['default', 'tutorial','default', 'tutorial','default', 'tutorial']}>
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec odio semper, aliquam ante id,
--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -6,14 +6,6 @@
     <title>Clayton Leonard Cook | Resume</title>
 </svelte:head>
 
-<h1>Resume</h1>
-
-<Window title="Default" tags={['default', 'tutorial']}>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec odio semper, aliquam ante id,
-		consequat ipsum.
-	</p>
-</Window>
 <Window title="Default" tags={['default', 'tutorial']}>
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec odio semper, aliquam ante id,

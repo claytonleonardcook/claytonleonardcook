901120f528ab7e735d43cc3c6f7ed00c73966d62
claytonleonardcook <claytonleonardcook+github@gmail.com>
2023-06-11 14:52:08 -0400

    feat: convert svelte to astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { z, defineCollection } from 'astro:content';
+import {z, defineCollection} from 'astro:content';
 
 // const blogCollection = defineCollection({ /* ... */ });
 
 const linksCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        tags: z.array(z.string()),
-        url: z.string(),
-        image: z.string(),
-    }),
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    url: z.string(),
+    image: z.string(),
+  }),
 });
 
 export const collections = {
-  'links': linksCollection,
+  links: linksCollection,
 };
